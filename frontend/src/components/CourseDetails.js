0f3add8f8cb26e23d5e1958900edc77d3742e6f1
Ilan Rothman <irothman123@gmail.com>
2023-05-24 21:51:23 +0300

    added delete button for all courses

--- frontend/src/components/CourseDetails.js
+++ frontend/src/components/CourseDetails.js
@@ -1,7 +1,16 @@
-
+import { useCoursesContext } from "../hooks/useCoursesContext";
 
 const CourseDetails = ({course}) => {
-    
+    const { dispatch } = useCoursesContext()
+    const handleClick = async () => {
+        const response = await fetch('/api/courses/' + course._id, {
+            method: 'DELETE'
+    })
+    const json = await response.json()
+    if (response.ok) {
+        dispatch({type: 'DELETE_COURSE', payload: json})
+    }
+}
     return (
         <div className="course-details">
             <h4>{course.CourseName}</h4>
@@ -10,6 +19,7 @@ const CourseDetails = ({course}) => {
             <br/>
             <p><strong>Details: </strong></p>
             <p>{course.CourseDetails}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 };
