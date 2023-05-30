e52667398b373f1d70fb6cb0796af67b8533b632
arbelhadari <arbelhadari@gmail.com>
2023-05-30 14:51:15 +0300

    added login and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import {
 
 // pages
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 // components
 import Navbar from "./components/Navbar";
@@ -18,6 +20,8 @@ function App() {
         <div className='pages'>
           <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/signup" element={<Signup />}/>
           </Routes>
         </div>
       </BrowserRouter>
